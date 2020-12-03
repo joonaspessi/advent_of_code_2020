ca08c6424e7fd3f122a105ef857b04d263e15d07
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-03 11:45:14 +0200

    day2 hotfix

--- tests/test_day2.py
+++ tests/test_day2.py
@@ -9,10 +9,12 @@ from day2_2 import app as day2_2
 def get_event_input(inputFilePath):
     with open(inputFilePath, 'r') as f:
         input_data = f.read()
+        lines = []
+        for line in input_data.splitlines():
+            lines.append(line)
+
     return {
-        "body": {
-            "input": input_data
-        }
+        "body": json.dumps({"input": lines})
     }
 
 
@@ -31,4 +33,4 @@ def test_day2_2(mocker):
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert data["result"] == 587
+    assert data["result"] == 352
