b07f8a14b12e8b29838752febb2f9d1405a830e7
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-04 16:59:37 +0200

    day 4

--- day4/app.py
+++ day4/app.py
@@ -1,27 +1,144 @@
 import json
+import re
+
+
+def get_passport_list(inputFilePath):
+    with open(inputFilePath, 'r') as f:
+        passports = []
+        passport = {}
+        for line in f:
+            line = line.rstrip()
+            if line == "":
+                passports.append(passport)
+                passport = {}
+            else:
+                attributes = line.split(" ")
+                for attribute in attributes:
+                    line_split = attribute.split(":")
+                    passport[line_split[0]] = line_split[1]
+        passports.append(passport)
+
+    return passports
+
+
+def is_valid_passport(passport):
+    if "byr" in passport and "iyr" in passport and "eyr" in passport and "hgt" in passport and "hcl" in passport and "ecl" in passport and "pid" in passport:
+        return True
+    return False
+
+
+def is_valid_byr(byr):
+    """ (Birth Year) - four digits; at least 1920 and at most 2002.
+    """
+    if re.match(r"^(19[2-9]\d|200[0-2])$", byr):
+        return True
+    return False
+
+
+def is_valid_iyr(iyr):
+    """ (Issue Year) - four digits; at least 2010 and at most 2020.
+    """
+    if re.match(r"^(201[0-9]|2020)$", iyr):
+        return True
+    return False
+
+
+def is_valid_eyr(eyr):
+    """ (Expiration Year) - four digits; at least 2020 and at most 2030.
+    """
+    if re.match(r"^(20[1-2][0-9]|2030)$", eyr):
+        return True
+    return False
+
+
+def is_valid_hgt(hgt):
+    """ (Height) - a number followed by either cm or in:
+        - If cm, the number must be at least 150 and at most 193.
+        - If in, the number must be at least 59 and at most 76.
+    """
+    if re.match(r"^(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$", hgt):
+        return True
+    return False
+
+
+def is_valid_hcl(hcl):
+    """ (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    """
+    if re.match(r"^#[0-9a-f]{6}$", hcl):
+        return True
+    return False
+
+
+def is_valid_ecl(ecl):
+    """ (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    """
+    if re.match(r"^(amb|blu|brn|gry|grn|hzl|oth)$", ecl):
+        return True
+    return False
+
+
+def is_valid_pid(pid):
+    """ (Passport ID) - a nine-digit number, including leading zeroes.
+    """
+    if re.match(r"^[0-9]{9}$", pid):
+        return True
+    return False
+
+
+def is_valid_passport_strict(passport):
+    if not is_valid_passport(passport):
+        return False
+    if not is_valid_byr(passport["byr"]):
+        return False
+    if not is_valid_iyr(passport["iyr"]):
+        return False
+    if not is_valid_eyr(passport["eyr"]):
+        return False
+    if not is_valid_hgt(passport["hgt"]):
+        return False
+    if not is_valid_hcl(passport["hcl"]):
+        return False
+    if not is_valid_ecl(passport["ecl"]):
+        return False
+    if not is_valid_pid(passport["pid"]):
+        return False
+
+    return True
 
 
 def lambda_handler(event, context):
     """Advent of code day 4, excercise 1
     """
-    body = json.loads(event["body"])
+    passports = get_passport_list("day4/input.txt")
+
+    valid_passports = 0
+    for passport in passports:
+        is_valid = is_valid_passport(passport)
+        if is_valid:
+            valid_passports += 1
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": 0
+            "result": valid_passports
         }),
     }
 
 
 def lambda_handler_2(event, context):
-    """Advent of code day 3, excercise 2
+    """Advent of code day 4, excercise 2
     """
-    body = json.loads(event["body"])
+    passports = get_passport_list("day4/input.txt")
+
+    valid_passports = 0
+    for passport in passports:
+        is_valid = is_valid_passport_strict(passport)
+        if is_valid:
+            valid_passports += 1
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": 0
+            "result": valid_passports
         }),
     }
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -7,19 +7,65 @@ from tests import get_event_input
 from day4 import app as day4
 
 
+def test_byr_validation():
+    assert day4.is_valid_byr("1919") == False
+    assert day4.is_valid_byr("1920") == True
+    assert day4.is_valid_byr("1999") == True
+    assert day4.is_valid_byr("2000") == True
+    assert day4.is_valid_byr("2002") == True
+    assert day4.is_valid_byr("2003") == False
+
+
+def test_iyr_validation():
+    assert day4.is_valid_iyr("2009") == False
+    assert day4.is_valid_iyr("2010") == True
+    assert day4.is_valid_iyr("2020") == True
+    assert day4.is_valid_iyr("2021") == False
+
+
+def test_eyr_validation():
+    assert day4.is_valid_eyr("1999") == False
+    assert day4.is_valid_eyr("2009") == False
+    assert day4.is_valid_eyr("2010") == True
+    assert day4.is_valid_eyr("2015") == True
+    assert day4.is_valid_eyr("2020") == True
+    assert day4.is_valid_eyr("2021") == True
+    assert day4.is_valid_eyr("2029") == True
+    assert day4.is_valid_eyr("2030") == True
+    assert day4.is_valid_eyr("2031") == False
+
+
+def test_hgt_validation():
+    assert day4.is_valid_hgt("58in") == False
+    assert day4.is_valid_hgt("59in") == True
+    assert day4.is_valid_hgt("61in") == True
+    assert day4.is_valid_hgt("76in") == True
+    assert day4.is_valid_hgt("77in") == False
+
+    assert day4.is_valid_hgt("149cm") == False
+    assert day4.is_valid_hgt("150cm") == True
+    assert day4.is_valid_hgt("180cm") == True
+    assert day4.is_valid_hgt("193cm") == True
+    assert day4.is_valid_hgt("194cm") == False
+    assert day4.is_valid_hgt("200cm") == False
+
+
+def test_hcl_validation():
+    assert day4.is_valid_hcl("#123abc") == True
+    assert day4.is_valid_hcl("123abc") == False
+
+
 def test_day4_1_given_input(mocker):
-    event = get_event_input("day4/input.txt")
-    ret = day4.lambda_handler(event, "")
+    ret = day4.lambda_handler({}, "")
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert data["result"] == 0
+    assert data["result"] == 254
 
 
 def test_day3_2_given_input(mocker):
-    event = get_event_input("day4/input.txt")
-    ret = day4.lambda_handler_2(event, "")
+    ret = day4.lambda_handler_2({}, "")
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert data["result"] == 0
+    assert data["result"] == 184
