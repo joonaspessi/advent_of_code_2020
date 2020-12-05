406ab5560affc18587293e2eaab180f78a069318
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-05 09:55:21 +0200

    day5

---
+++ day5/app.py
@@ -0,0 +1,90 @@
+import json
+import math
+
+
+def get_input(event):
+    with open("day5/input.txt", 'r') as f:
+        lines = f.readlines()
+        lines = map(lambda n: n.rstrip(), lines)
+    return lines
+
+
+def get_row(row, lower_limit, upper_limit):
+    next = row[0]
+    if (len(row) > 1):
+        mid_limit = int(lower_limit + (1 + upper_limit-lower_limit) / 2)
+        if next == "F":
+            return get_row(row[1:], lower_limit, mid_limit - 1)
+        else:
+            return get_row(row[1:], mid_limit, upper_limit)
+    else:
+        if next == "F":
+            return lower_limit
+        else:
+            return upper_limit
+
+
+def get_column(column, lower_limit, upper_limit):
+    next = column[0]
+    if (len(column) > 1):
+        mid_limit = int(lower_limit + (1 + upper_limit-lower_limit) / 2)
+        if next == "L":
+            return get_column(column[1:], lower_limit, mid_limit - 1)
+        else:
+            return get_column(column[1:], mid_limit, upper_limit)
+    else:
+        if next == "L":
+            return lower_limit
+        else:
+            return upper_limit
+
+
+def get_seat_id(row_id, column_id, magic_multiplier=8):
+    return row_id * magic_multiplier + column_id
+
+
+def lambda_handler(event, _context):
+    boarding_passes = get_input(event)
+
+    highest_seat_id = 0
+    for boarding_pass in boarding_passes:
+        row_id = get_row(boarding_pass[:7], 0, 127)
+        column_id = get_column(boarding_pass[7:], 0, 7)
+        seat_id = get_seat_id(row_id, column_id)
+        if seat_id > highest_seat_id:
+            highest_seat_id = seat_id
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "result": highest_seat_id
+        }),
+    }
+
+
+def lambda_handler_2(event, _context):
+    boarding_passes = get_input(event)
+
+    seat_ids = []
+    for boarding_pass in boarding_passes:
+        row_id = get_row(boarding_pass[:7], 0, 127)
+        column_id = get_column(boarding_pass[7:], 0, 7)
+        seat_id = get_seat_id(row_id, column_id)
+        seat_ids.append(seat_id)
+    seat_ids_sorted = sorted(seat_ids)
+    min_id = min(seat_ids_sorted)
+    max_id = max(seat_ids_sorted)
+    seat_range = list(range(min_id, max_id+1))
+
+    missing_seat_id = 0
+    for id in seat_range:
+        if id not in seat_ids_sorted:
+            missing_seat_id = id
+            break
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "result": missing_seat_id
+        }),
+    }
---
+++ tests/test_day5.py
@@ -0,0 +1,30 @@
+import json
+
+import pytest
+
+from day5 import app as day5
+from tests import get_event_input
+
+
+def test_get_row():
+    assert day5.get_row("FFFBBBF", 0, 127) == 14
+    assert day5.get_row("FBFBBFF", 0, 127) == 44
+    assert day5.get_row("BFFFBBF", 0, 127) == 70
+    assert day5.get_row("BBFFBBF", 0, 127) == 102
+
+
+def test_get_column():
+    assert day5.get_column("RRR", 0, 7) == 7
+    assert day5.get_column("RLL", 0, 7) == 4
+
+
+def test_day5_1():
+    ret = day5.lambda_handler({}, {})
+    data = json.loads(ret["body"])
+    assert data.get("result") == 806
+
+
+def test_day5_2():
+    ret = day5.lambda_handler_2({}, {})
+    data = json.loads(ret["body"])
+    assert data.get("result") == 562
