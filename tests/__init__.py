b7fe15b270fe76e49797a5a4f5ade9e81446439a
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-08 19:39:58 +0200

    clean test utility

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,6 @@
 import json
 
 
-def get_event_input(inputFilePath):
-    with open(inputFilePath, 'r') as f:
-        input_data = f.read()
-        lines = []
-        for line in input_data.splitlines():
-            lines.append(line)
-
-    print({"input": lines})
-    return {
-        "body": json.dumps({"input": lines})
-    }
-
-
 def get_event_input_raw(inputFilePath):
     with open(inputFilePath, 'r') as f:
         input_data = f.read()
