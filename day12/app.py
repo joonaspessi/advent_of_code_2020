ca3965b6067d3bbd15268a230cc69ae4c7fde679
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-12 15:10:22 +0200

    clean day12

--- day12/app.py
+++ day12/app.py
@@ -13,29 +13,36 @@ def get_input(event):
 
 def lambda_handler(event, _):
     instructions = get_input(event)
-    dir_to_angle = {"N": 0, "E": 90, "S": 180, "W": 270}
-    angle_to_dir = {0: "N", 90: "E", 180: "S", 270: "W"}
-    cur_dir = "E"
-    movements = {"N": 0, "E": 0, "S": 0, "W": 0}
+    dir = [1, 0]
+    ship = [0, 0]
     for x in instructions:
-        if x[0] == "E" or x[0] == "S" or x[0] == "W" or x[0] == "N":
-            movements[x[0]] += x[1]
+        if x[0] == "E":
+            ship[0] += x[1]
+        elif x[0] == "S":
+            ship[1] -= x[1]
+        elif x[0] == "W":
+            ship[0] -= x[1]
+        elif x[0] == "N":
+            ship[1] += x[1]
         elif x[0] == "F":
-            movements[cur_dir] += x[1]
-        elif x[0] == "R":
-            new_degree = (dir_to_angle[cur_dir] + x[1]) % 360
-            cur_dir = angle_to_dir[new_degree]
-        elif x[0] == "L":
-            new_degree = (dir_to_angle[cur_dir] - x[1]) % 360
-            cur_dir = angle_to_dir[new_degree]
+            move = list(map(lambda n: n * x[1], dir))
+            ship = [i + y for i, y in zip(move, ship)]
+        elif x[0] == "R" or "L":
+            angle = math.radians(
+                x[1]) if x[0] == "L" else -1 * math.radians(x[1])
+            new_x = round(dir[0] * math.cos(angle) -
+                          dir[1] * math.sin(angle))
+            new_y = round(dir[0] * math.sin(angle) +
+                          dir[1] * math.cos(angle))
+            dir = [new_x, new_y]
+        print(f"move={x}, dir={dir}, ship={ship}")
 
-    mov = abs(movements["N"] - movements["S"]) + \
-        abs(movements["E"] - movements["W"])
+    manhattan = abs(ship[0]) + abs(ship[1])
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": mov
+            "result": manhattan
         }),
     }
 
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -8,14 +8,14 @@ from tests import get_event_input_raw
 
 def test_day12_1_test():
     event = get_event_input_raw("day12/input_test.txt")
-    ret = day12.lambda_handler(event, {})
+    ret = day12.lambda_handler_3(event, {})
     data = json.loads(ret["body"])
     assert data.get("result") == 25
 
 
 def test_day12_1():
     event = get_event_input_raw("day12/input.txt")
-    ret = day12.lambda_handler(event, {})
+    ret = day12.lambda_handler_3(event, {})
     data = json.loads(ret["body"])
     assert data.get("result") == 1603
 
