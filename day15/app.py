e1f01eaf5bdbb6a5e3c01e8f25d1953740ac2fe7
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-15 15:53:51 +0200

    Day15

--- day15/app.py
+++ day15/app.py
@@ -12,75 +12,47 @@ def get_input(event):
 
 def lambda_handler(event, _):
     input = get_input(event)
-    start = [int(n) for n in input.split(",")]
+    turns = [int(n) for n in input.split(",")]
+    last_lookup = {}
 
-    mem = {}
-    turns = []
-    for s in start:
-        turns.append(s)
-        if s not in mem:
-            mem[s] = []
-        mem[s].append(len(turns))
+    for i, num in enumerate(turns[:-1]):
+        last_lookup[num] = i
 
-    for turn in range(len(turns) + 1, 2020 + 1):
-        prev = turns[-1]
-
-        if prev not in turns[:-1]:
-            turns.append(0)
-            if 0 not in mem:
-                mem[0] = []
-            mem[0].append(turn)
-        elif len(mem[prev]) >= 2:
-            new_val = mem[prev][-1] - mem[prev][-2]
-            turns.append(new_val)
-            if new_val not in mem:
-                mem[new_val] = []
-            mem[new_val].append(turn)
-        else:
-            new_val = turn - mem[prev][0]
-            turns.append(new_val)
-            if new_val not in mem:
-                mem[new_val] = []
-            mem[new_val].append(turn)
-
+    while len(turns) < 2020:
+        turn_index = len(turns) - 1
+        previous = turns[-1]
+        next_val = turn_index - last_lookup.get(previous, turn_index)
+        last_lookup[previous] = turn_index
+        turns.append(next_val)
     result = turns[-1:]
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": result
+            "result": result[0]
         })
     }
 
 
 def lambda_handler_2(event, _):
     input = get_input(event)
-    start = [int(n) for n in input.split(",")]
+    turns = [int(n) for n in input.split(",")]
+    last_lookup = {}
 
-    mem = {}
-    turns = []
-    for s in start:
-        turns.append(s)
-        if s not in mem:
-            mem[s] = []
-        mem[s].append(len(turns))
+    for i, num in enumerate(turns[:-1]):
+        last_lookup[num] = i
 
-    for turn in range(len(turns) + 1, 30000000 + 1):
-        prev = turns[-1]
+    while len(turns) < 30000000:
+        index = len(turns) - 1
+        previous = turns[-1]
+        next_val = index - last_lookup.get(previous, index)
+        last_lookup[previous] = index
+        turns.append(next_val)
+    result = turns[-1:]
 
-        if prev not in turns[:-1]:
-            turns.append(0)
-            if 0 not in mem:
-                mem[0] = []
-            mem[0].append(turn)
-        elif len(mem[prev]) >= 2:
-            new_val = mem[prev][-1] - mem[prev][-2]
-            turns.append(new_val)
-            if new_val not in mem:
-                mem[new_val] = []
-            mem[new_val].append(turn)
-        else:
-            new_val = turn - mem[prev][0]
-            turns.append(new_val)
-            if new_val not in mem:
-                mem[new_val] = []
-            mem[new_val].append(turn)
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "result": result[0]
+        })
+    }
--- tests/test_day15.py
+++ tests/test_day15.py
@@ -10,25 +10,25 @@ def test_day15_1_test():
     event = get_event_input_raw("day15/input_test.txt")
     ret = day15.lambda_handler(event, {})
     data = json.loads(ret["body"])
-    assert data.get("result") == 165
+    assert data.get("result") == 436
 
 
 def test_day15_1():
     event = get_event_input_raw("day15/input.txt")
     ret = day15.lambda_handler(event, {})
     data = json.loads(ret["body"])
-    assert data.get("result") == 6559449933360
+    assert data.get("result") == 447
 
 
 def test_day15_2_test():
     event = get_event_input_raw("day15/input_test2.txt")
     ret = day15.lambda_handler_2(event, {})
     data = json.loads(ret["body"])
-    assert data.get("result") == 208
+    assert data.get("result") == 2578
 
 
 def test_day15_2():
     event = get_event_input_raw("day15/input.txt")
     ret = day15.lambda_handler_2(event, {})
     data = json.loads(ret["body"])
-    assert data.get("result") == 3369767240513
+    assert data.get("result") == 11721679
