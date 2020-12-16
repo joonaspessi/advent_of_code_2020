cf20ad42e0ed147c4252c1703726979dc1cf10d6
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-16 18:47:49 +0200

    day16 part 2

--- day16/app.py
+++ day16/app.py
@@ -42,10 +42,69 @@ def lambda_handler(event, _):
 
 def lambda_handler_2(event, _):
     input = get_input(event)
+    cons, mt, nt = input.split("\n\n")
+    reg = r"^(.+)\: (\d+)-(\d+) or (\d+)-(\d+)$"
+    constrains = []
+    rules = {}
+    for c in cons.splitlines():
+        c = c.rstrip()
+        res = re.search(reg, c)
+        constrains.append(range(int(res[2]), int(res[3])+1))
+        constrains.append(range(int(res[4]), int(res[5])+1))
+
+        rules[res[1]] = constrains[-2:]
+
+    valid_tickets = []
+    for ticket_raw in nt[nt.index(":") + 2:].splitlines():
+        ticket = [int(a) for a in ticket_raw.split(",")]
+        ok = True
+        for t in ticket:
+            field_ok = False
+            for c in constrains:
+                if t in c:
+                    field_ok = True
+            if not field_ok:
+                ok = False
+        if ok:
+            valid_tickets.append(ticket)
+
+    col_len = len(valid_tickets[0])
+    row_len = len(valid_tickets)
+
+    graph = {}
+    for name, constrains in rules.items():
+        for c in range(col_len):
+            valid = True
+            for r in range(row_len):
+                ticket_field = valid_tickets[r][c]
+                field_ok = False
+                for constrain in constrains:
+                    if ticket_field in constrain:
+                        field_ok = True
+                if not field_ok:
+                    valid = False
+                    break
+            if valid:
+                graph[c] = graph.get(c, []) + [name]
+
+    found = 0
+    order = [False for _ in range(col_len)]
+    while found < col_len:
+        for c in range(col_len):
+            possible = [i for i in graph[c] if i not in order]
+            if len(possible) == 1:
+                found += 1
+                order[c] = possible[0]
+
+    product = 1
+    my_ticket = re.findall(r"\d+", mt)
+    for i, o in enumerate(order):
+        if "departure" in o:
+            product *= int(my_ticket[i])
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": 0
+            "result": product
         })
     }
--- tests/test_day16.py
+++ tests/test_day16.py
@@ -20,15 +20,8 @@ def test_day16_1():
     assert data.get("result") == 26053
 
 
-def test_day16_2_test():
-    event = get_event_input_raw("day16/input_test2.txt")
-    ret = day16.lambda_handler_2(event, {})
-    data = json.loads(ret["body"])
-    assert data.get("result") == 2578
-
-
 def test_day16_2():
     event = get_event_input_raw("day16/input.txt")
     ret = day16.lambda_handler_2(event, {})
     data = json.loads(ret["body"])
-    assert data.get("result") == 11721679
+    assert data.get("result") == 1515506256421
