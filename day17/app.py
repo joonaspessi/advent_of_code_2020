3cc0452b691a47b71f49887a677a6e318f8d23b3
Joonas Pessi <joonas.pessi@futurice.com>
2020-12-17 20:48:14 +0200

    day17

--- day17/app.py
+++ day17/app.py
@@ -1,5 +1,4 @@
 import json
-import re
 
 
 def get_input(event):
@@ -11,82 +10,81 @@ def get_input(event):
     return lines
 
 
-def empty_plane(x_len, y_len):
-    p = []
-    for y in range(y_len):
-        for x in range(x_len):
-            p.append(".")
-    return p
-
-
 def lambda_handler(event, _):
     input = get_input(event)
     lines = input.splitlines()
-    matrix = []
 
-    for y, line in enumerate(lines):
-        matrix.append([[cube] for cube in line])
+    matrix = set()
 
-    x_len = len(matrix[0])
-    y_len = len(matrix)
+    for y, row in enumerate(lines):
+        for x, char in enumerate(row):
+            if char == "#":
+                matrix.add((y, x, 0))
 
-    for round in range(6):
-        for y in range(y_len):
-            for x in range(x_len):
-                
+    max_range = 15  # len(lines) + 6 + 1
+    for _ in range(6):
+        new_matrix = set()
+        for y in range(-max_range, max_range):
+            for x in range(-max_range, max_range):
+                for z in range(-max_range, max_range):
+                    cube_on = 0
+                    for dy in [-1, 0, 1]:
+                        for dx in [-1, 0, 1]:
+                            for dz in [-1, 0, 1]:
+                                if dy != 0 or dx != 0 or dz != 0:
+                                    if (y + dy, x + dx, z + dz) in matrix:
+                                        cube_on += 1
+                    if cube_on == 3 and (y, x, z) not in matrix:
+                        new_matrix.add((y, x, z))
+                    if cube_on in [2, 3] and (y, x, z) in matrix:
+                        new_matrix.add((y, x, z))
+        matrix = new_matrix
 
-        for y in range(y_len):
-            for x in range(x_len):
-                matrix[y][x].append(".")
-
-        z_len = len(matrix[0][0][0])
-
-        activate = []
-        for y in range(y_len):
-            for x in range(x_len):
-                for z in range(z_len):
-                    active_cubes = 0
-                    for yy in [-1, 0, 1]:
-                        for xx in [-1, 0, 1]:
-                            for zz in [-1, 0, 1]:
-                                if xx != 0 and yy != 0 and zz != 0:
-                                    if 0 >= y + yy <= y_len and 0 >= xx + x <= x_len and 0 >= zz + z <= z_len:
-                                        if matrix[y+yy][x+xx][z+zz] == "#":
-                                            active_cubes += 1
-                    if not(2 <= active_cubes <= 3) and matrix[y][x][z] == "#":
-                        activate.append([y, x, z, "."])
-                    elif matrix[y][x][z] == "." and active_cubes == 3:
-                        activate.append([y, x, z, "#"])
-
-        for a in activate:
-            y = a[0]
-            x = a[1]
-            z = a[2]
-            state = a[3]
-            matrix[y][x][z] = state
-
-    z_len = len(matrix[0][0][0])
-    count = 0
-    for y, _ in enumerate(y_len):
-        for x, _ in enumerate(x_len):
-            for z, _ in enumerate(z_len):
-                if matrix[y][x][z] == "#":
-                    count += 1
-
+    result = len(matrix)
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": 0
+            "result": result
         })
     }
 
 
 def lambda_handler_2(event, _):
     input = get_input(event)
+    lines = input.splitlines()
 
+    matrix = set()
+
+    for y, row in enumerate(lines):
+        for x, char in enumerate(row):
+            if char == "#":
+                matrix.add((y, x, 0, 0))
+
+    max_range = len(lines) + 6 + 1
+    for _ in range(6):
+        new_matrix = set()
+        for y in range(-max_range, max_range):
+            for x in range(-max_range, max_range):
+                for z in range(-max_range, max_range):
+                    for w in range(-max_range, max_range):
+                        cube_on = 0
+                        for dy in [-1, 0, 1]:
+                            for dx in [-1, 0, 1]:
+                                for dz in [-1, 0, 1]:
+                                    for dw in [-1, 0, 1]:
+                                        if dy != 0 or dx != 0 or dz != 0 or dw != 0:
+                                            if (y + dy, x + dx, z + dz, w + dw) in matrix:
+                                                cube_on += 1
+                        if cube_on == 3 and (y, x, z, w) not in matrix:
+                            new_matrix.add((y, x, z, w))
+                        if cube_on in [2, 3] and (y, x, z, w) in matrix:
+                            new_matrix.add((y, x, z, w))
+        matrix = new_matrix
+
+    result = len(matrix)
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "result": 0
+            "result": result
         })
     }
